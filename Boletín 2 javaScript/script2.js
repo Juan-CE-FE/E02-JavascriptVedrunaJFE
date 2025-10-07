1029acb5db6c695489a28df47b2265456da57ba4
Juan Cortes Fernández Escolastico <dam24-11@vedrunacentroseducativos.onmicrosoft.com>
2025-10-07 11:09:23 +0200

    Primer commit

---
+++ Boletín 2 javaScript/script2.js
@@ -0,0 +1,20 @@
+
+
+//1.Cambiar el color de un párrafo al hacer clic en un botón.
+document.addEventListener('DOMContentLoaded', () => {
+    let mainDiv = document.getElementById("title");
+    let button = document.getElementById("button");
+    button.addEventListener("click", () => {
+        mainDiv.style.color = "red";
+    });
+});
+
+
+//2.Crear un párrafo nuevo dentro de un div.
+
+//3.Eliminar el último elemento de una lista con un botón.
+
+//4.Cambiar el texto de un título (h1) cuando el ratón pase por encima.
+
+//5.Ocultar y mostrar un div con un botón.
+
